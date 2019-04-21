a9152eb0342e0615a21eeb7bbe9472b7ae7900e7
FlorianCahay <38866797+FlorianCahay@users.noreply.github.com>
2019-04-21 22:16:43 +0200

    Ajout d'une alarme sonore

--- app/src/main/java/com/example/countdown/EndCountdown.java
+++ app/src/main/java/com/example/countdown/EndCountdown.java
@@ -1,13 +1,34 @@
 package com.example.countdown;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import static android.media.RingtoneManager.TYPE_ALARM;
 
 public class EndCountdown extends AppCompatActivity {
+    private MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_countdown);
+        Uri actualDefaultRingtoneUri = RingtoneManager.getActualDefaultRingtoneUri(this, TYPE_ALARM);
+        player = MediaPlayer.create(this, actualDefaultRingtoneUri);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        player.setLooping(true);
+        player.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.stop();
     }
 }
